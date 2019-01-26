f3b90046eadad20efdf3496bb28938250de5af68
mdctleo <31061195+mdctleo@users.noreply.github.com>
2019-01-26 12:59:49 -0800

    Added some dependencies

--- app.js
+++ app.js
@@ -3,16 +3,26 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mysql = require('mysql');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
+var cors = require('cors');
+var corsOptions = {
+    origin: true,
+    credentials: true,
+    optionsSuccessStatus: 200
+};
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(cors(corsOptions));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
