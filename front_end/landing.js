da3d94bd3e977da263c15e1d48f5a787526c26c5
mdctleo <31061195+mdctleo@users.noreply.github.com>
2019-01-26 21:48:32 -0800

    front-end first request + some skeletons

--- front_end/landing.js
+++ front_end/landing.js
@@ -12,8 +12,12 @@ angular.module('myApp.landing', ['ngRoute'])
     .controller('LandingCtrl', [function() {
         let vm = this;
         vm.pictures = [];
+        vm.currPic = "./img/destinations/alaska.jpg";
+        vm.tags = {};
+        vm.addTag = addTag;
+        vm.removeTag = removeTag;
+        vm.nextPic = nextPic;
         (function initLanding() {
-            console.log("got here");
             let url = "http://127.0.0.1:3000/selectPhaseOnePictures";
             fetch(url, {
                 method: "GET",
@@ -26,12 +30,27 @@ angular.module('myApp.landing', ['ngRoute'])
                 return response.json()
             }).then(pictures => {
                 vm.pictures = pictures;
+                vm.currPic = vm.pictures[0];
+                console.log(vm.currPic);
 
             }).catch(error => {
                 console.log(error.message);
             })
 
         })();
+
+
+        function addTag() {
+
+        }
+
+        function removeTag() {
+
+        }
+
+        function nextPic($event) {
+
+        }
 
 
 
