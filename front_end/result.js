037fe716f9a38b3e97342508a1b12eae31b1e842
Mario Grebelski <grebelsm@Marios-MacBook-Pro.local>
2019-01-27 00:34:17 -0800

    Result page and updated tables

--- front_end/result.js
+++ front_end/result.js
@@ -10,5 +10,28 @@ angular.module('myApp.result', ['ngRoute'])
     }])
 
     .controller('ResultCtrl', [function() {
+        let vm = this;
+        vm.currPic = "./img/destinations/alaska.jpg";
+        (function initLanding() {
+            let url = "http://127.0.0.1:3000/selectPhaseOnePictures";
+            fetch(url, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                credentials: "same-origin"
+            }).then(response => {
+
+                return response.json()
+            }).then(pictures => {
+                vm.pictures = pictures;
+                vm.currPic = vm.pictures[0];
+                console.log(vm.currPic);
+
+            }).catch(error => {
+                console.log(error.message);
+            })
+
+        })();
 
     }]);
